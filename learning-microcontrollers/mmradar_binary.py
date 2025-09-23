fdb00415ff448940e0c37b57e8db01f5d2bfaee2
gsam1 <gsamardjiev@gmail.com>
2025-09-23 19:45:26 +0300

    Trying out a dual radar system

--- learning-microcontrollers/mmradar_binary.py
+++ learning-microcontrollers/mmradar_binary.py
@@ -1,40 +1,54 @@
 import machine
-import utime
 import binascii
 
-tx_pin_number = 4
-rx_pin_number = 5
+txa_pin_number = 0
+rxa_pin_number = 1
+txb_pin_number = 4
+rxb_pin_number = 5
 
-tx_pin = machine.Pin(tx_pin_number)
-rx_pin = machine.Pin(rx_pin_number)
 
-soft_uart = machine.UART(1, baudrate=115200, tx=tx_pin, rx=rx_pin)
+txa_pin = machine.Pin(txa_pin_number)
+rxa_pin = machine.Pin(rxa_pin_number)
+txb_pin = machine.Pin(txb_pin_number)
+rxb_pin = machine.Pin(rxb_pin_number)
+
+
+soft_uart_a = machine.UART(0, baudrate=115200, tx=txa_pin, rx=rxa_pin)
+soft_uart_b = machine.UART(1, baudrate=115200, tx=txb_pin, rx=rxb_pin)
 
 def send_hex_string(hex_string):
     hex_bytes = binascii.unhexlify(hex_string)
-    soft_uart.write(hex_bytes)
+    print("Sending to a")
+    soft_uart_a.write(hex_bytes)
+    print("Sending to b")
+    soft_uart_b.write(hex_bytes)
+
 
 def read_serial_data():
     print("Reading serial data...")
-    buffer = []
-    counter = 0
     while True:
-        if soft_uart.any():
-            data = soft_uart.readline()
-            if data == b'ON\r\n':
-                buffer.append(1)
-            elif data == b'OFF\r\n':
-                buffer.append(0)
+        if soft_uart_a.any():
+            dataa = soft_uart_a.read()
+            print(dataa)
+        
+        if soft_uart_b.any():
+            datab = soft_uart_b.read()
+            print(datab)
+            # data = dataa + datab
+            # if data == b'ON\r\n':
+            #     buffer.append(1)
+            # elif data == b'OFF\r\n':
+            #     buffer.append(0)
             
-            if len(buffer) >= 10:
-                buffer_sum = sum(buffer)
-                if buffer_sum >= 3:
-                    print("Presence Detected.", buffer_sum, counter)
-                else:
-                    print("No Presence Detected.", buffer_sum, counter)
+            # if len(buffer) >= 10:
+            #     buffer_sum = sum(buffer)
+            #     if buffer_sum >= 3:
+            #         print("Presence Detected.", buffer_sum, counter)
+            #     else:
+            #         print("No Presence Detected.", buffer_sum, counter)
                 
-                counter += 1
-                buffer = []
+            #     counter += 1
+            #     buffer = []
                
 
 if __name__ == "__main__":
